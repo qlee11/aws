d1807ad27eae146a3b19a472793e932693e634b6
Kristóf Kuli <kuli.kristof@gmail.hu>
2026-05-25 10:38:05 +0200

    Switched to vpc_security_group_ids.

--- machine.tf
+++ machine.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "tavernquest" {
   ami             = var.ami
   instance_type   = var.machine_type
-  security_groups = [resource.aws_security_group.allow_ssh.id]
-  
+  vpc_security_group_ids = [resource.aws_security_group.allow_ssh.id]
+
   primary_network_interface {
     network_interface_id = resource.aws_network_interface.NIC.id
   }
