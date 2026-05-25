b63bcf7e5b6ffc258acef536855671bcbf0659fe
Kristóf Kuli <kuli.kristof@gmail.hu>
2026-05-25 10:53:16 +0200

    Exclude network interface

--- machine.tf
+++ machine.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "tavernquest" {
   ami             = var.ami
   instance_type   = var.machine_type
+  #subnet_id = aws_subnet.test-subnet.id
   vpc_security_group_ids = [resource.aws_security_group.allow_ssh.id]
 
-  primary_network_interface {
-    network_interface_id = resource.aws_network_interface.test-nic.id
-  }
+  # primary_network_interface {
+  #   network_interface_id = resource.aws_network_interface.test-nic.id
+  # }
 }
