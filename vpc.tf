2ee315e5d02be7ec2d29546fc758e37df8500f3f
Kristóf Kuli <kuli.kristof@gmail.hu>
2026-05-25 10:26:33 +0200

    Added subnet.

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,12 @@ resource "aws_vpc" "test-network" {
   cidr_block = "172.16.0.0/16"
 }
 
+resource "aws_subnet" "test-subnet"{
+  vpc_id = aws_vpc.test-network.id
+  cidr_block = "172.16.10.0/24"
+}
+
 resource "aws_network_interface" "NIC" {
-  subnet_id   = resource.aws_vpc.test-network.id
+  subnet_id   = resource.aws_vpc.test-subnet.id
   private_ips = ["172.16.10.100"]
 }
