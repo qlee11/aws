be43b2cf32a37250e1b110b79bce8eab9201305f
Kristóf Kuli <kuli.kristof@gmail.hu>
2026-05-26 17:31:32 +0200

    Removed nic

--- vpc.tf
+++ vpc.tf
@@ -6,8 +6,3 @@ resource "aws_subnet" "test-subnet" {
   vpc_id     = aws_vpc.test-network.id
   cidr_block = "192.168.1.0/24"
 }
-
-resource "aws_network_interface" "test-nic" {
-  subnet_id       = resource.aws_subnet.test-subnet.id
-  security_groups = [resource.aws_security_group.allow_ssh.id]
-}
