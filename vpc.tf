77fec16fbbbe37eb743f8920f93ae05207f994a0
Kristóf Kuli <kuli.kristof@gmail.hu>
2026-05-29 10:52:46 +0200

    Added route table resource.

--- vpc.tf
+++ vpc.tf
@@ -15,3 +15,12 @@ resource "aws_ec2_instance_connect_endpoint" "connect" {
   subnet_id = aws_subnet.test-subnet.id
   security_group_ids = [aws_security_group.allow_ssh.id]
 }
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.test-network.id
+  depends_on = [ aws_internet_gateway.gw.id ]
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
