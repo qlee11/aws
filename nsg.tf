9c17eeedf927da4b593d5556acc7ca648cb06296
Kristóf Kuli <kuli.kristof@gmail.hu>
2026-05-30 12:05:43 +0200

    Changed subnet IP range.

--- nsg.tf
+++ nsg.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "allow_ssh" {
 
 resource "aws_vpc_security_group_ingress_rule" "ssh" {
   security_group_id = resource.aws_security_group.allow_ssh.id
-  cidr_ipv4         = "192.168.1.0/24"
+  cidr_ipv4         = "172.16.128.0/24"
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
@@ -17,7 +17,9 @@ resource "aws_vpc_security_group_ingress_rule" "ssh" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
   security_group_id = resource.aws_security_group.allow_ssh.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = -1
+  from_port = 443
+  ip_protocol       = "tcp"
+  to_port = 8080
 }
 #--------------------------------------------
 
@@ -39,7 +41,7 @@ resource "aws_vpc_security_group_ingress_rule" "private" {
 
 resource "aws_vpc_security_group_egress_rule" "ec2" {
   security_group_id = aws_security_group.allow_instance_connect_endpoint.id
-  cidr_ipv4         = "192.168.1.0/24"
+  cidr_ipv4         = "172.16.128.0/24"
   from_port = 22
   ip_protocol       = "tcp"
   to_port = 22
--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,12 @@
 # VPC configuration
 #------------------------------------
 resource "aws_vpc" "test-network" {
-  cidr_block = "192.168.0.0/23"
+  cidr_block = "172.16.0.0/24"
 }
 
 resource "aws_subnet" "test-subnet" {
   vpc_id     = aws_vpc.test-network.id
-  cidr_block = "192.168.1.0/24"
+  cidr_block = "172.16.128.0/24"
 }
 #------------------------------------
 
