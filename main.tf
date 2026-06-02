0ba124d60382b6b03ca9366344787e586b741174
Kristóf Kuli <kuli.kristof@gmail.hu>
2026-06-02 12:39:48 +0200

    Added eks configuration.

--- main.tf
+++ main.tf
@@ -32,3 +32,26 @@ module "ecr" {
   })
 
 }
+
+resource "aws_eks_cluster" "text-cluster" {
+    name = "test-custer"
+    role_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+
+    vpc_config {
+      subnet_ids = [ aws_subnet.test-subnet.id ]
+    }
+
+}
+
+resource "aws_eks_node_group" "test-node-group" {
+    cluster_name = aws_eks_cluster.text-cluster.name
+    node_group_name = "test-node-group"
+    node_role_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+    subnet_ids = [ aws_subnet.test-subnet.id ]
+    
+    scaling_config {
+      desired_size = 1
+      max_size     = 2
+      min_size     = 1
+    }
+}
