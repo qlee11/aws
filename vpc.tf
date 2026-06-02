ec4ce32d122343ca3b172247117a15be989d3c2f
Kristóf Kuli <kuli.kristof@gmail.hu>
2026-06-02 14:01:02 +0200

    Testign AZs.

--- vpc.tf
+++ vpc.tf
@@ -1,23 +1,24 @@
 # VPC configuration
 #------------------------------------
 resource "aws_vpc" "test-network" {
-  cidr_block = "172.16.0.0/24"
+  cidr_block = "172.16.0.0/16"
 }
 
 resource "aws_subnet" "test-subnet" {
   vpc_id     = aws_vpc.test-network.id
-  cidr_block = "172.16.0.64/28"
+  cidr_block = "172.16.1.0/24"
+  availability_zone = "eu-central-1a"
 }
 
 resource "aws_subnet" "cluster-subnet" {
   vpc_id     = aws_vpc.test-network.id
-  cidr_block = "172.16.0.128/28"
+  cidr_block = "172.16.2.0/24"
   availability_zone = "eu-central-1b"
 }
 
 resource "aws_network_interface" "test-nic" {
   subnet_id   = aws_subnet.test-subnet.id
-  private_ips = ["172.16.0.10"]
+  private_ips = ["172.16.0.129"]
 
   tags = {
     Name = "primary_network_interface"
