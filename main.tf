5dafd182ced0f1a416bcac3dcdb85cb512a0d006
Kristóf Kuli <kuli.kristof@gmail.hu>
2026-06-09 10:44:58 +0200

    Removed eks configuration.

--- main.tf
+++ main.tf
@@ -33,49 +33,49 @@ module "ecr" {
 
 }
 
-resource "aws_eks_cluster" "test-cluster" {
-    name = "test-custer"
-    role_arn = aws_iam_role.cluster.arn
+# resource "aws_eks_cluster" "test-cluster" {
+#     name = "test-custer"
+#     role_arn = aws_iam_role.cluster.arn
 
-    vpc_config {
-      subnet_ids = [ aws_subnet.test-subnet.id, aws_subnet.cluster-subnet.id ]
-    }
+#     vpc_config {
+#       subnet_ids = [ aws_subnet.test-subnet.id, aws_subnet.cluster-subnet.id ]
+#     }
 
-}
+# }
 
-resource "aws_eks_node_group" "test-node-group" {
-    cluster_name = aws_eks_cluster.test-cluster.name
-    node_group_name = "test-node-group"
-    node_role_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-    subnet_ids = [ aws_subnet.test-subnet.id ]
+# resource "aws_eks_node_group" "test-node-group" {
+#     cluster_name = aws_eks_cluster.test-cluster.name
+#     node_group_name = "test-node-group"
+#     node_role_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+#     subnet_ids = [ aws_subnet.test-subnet.id ]
 
-    scaling_config {
-      desired_size = 1
-      max_size     = 2
-      min_size     = 1
-    }
-}
+#     scaling_config {
+#       desired_size = 1
+#       max_size     = 2
+#       min_size     = 1
+#     }
+# }
 
-resource "aws_iam_role" "cluster" {
-  name = "eks-cluster-example"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "sts:AssumeRole",
-          "sts:TagSession"
-        ]
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
+# resource "aws_iam_role" "cluster" {
+#   name = "eks-cluster-example"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = [
+#           "sts:AssumeRole",
+#           "sts:TagSession"
+#         ]
+#         Effect = "Allow"
+#         Principal = {
+#           Service = "eks.amazonaws.com"
+#         }
+#       },
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.cluster.name
-}
+# resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+#   role       = aws_iam_role.cluster.name
+# }
